d9435375a38e38bc8ebf30e72e0fa0e57381451f
Luke Darragh <darraghl@coventry.ac.uk>
2017-11-15 11:30:46 +0000

    Added function call to server code

--- server_code.py
+++ server_code.py
@@ -2,34 +2,41 @@ import socket
 import time
 import random
 from imdb import imdb
+from MovieID import movieSearch
 i=imdb.IMDb(accessSystem='http')
  
- 
-def Main():
-    #Give ChatBot server an IP address and port
-    host = "127.0.0.1"
-    port = 5001
-    #Create Socket and bind server to socket           
-    thisSocket = socket.socket()
-    thisSocket.bind((host,port))
-    #Listen for clients     
-    thisSocket.listen(1)
-    #Connect to client
-    conn, addr = thisSocket.accept()
-    #print Connect ip address
-    print ("The Connection ip is : " + str(addr))
-    #Repear forever
-    while True:
-                #Receive info from client
-                receiveMess = conn.recv(1024).decode()
-                #if no info from client end loop
-                if not receiveMess:
-                                    break
-                #Print info from client
-                print ("Message from User to Chatbot : " + str(receiveMess))
-                #set return message
-                returnMess = "This is the return message"
-                conn.send(returnMess.encode())                             
-    conn.close()                
+ def Main():
+	#Give ChatBot server an IP address and port
+	host = "127.0.0.1"
+	port = 5001
+	#Create Socket and bind server to socket           
+	thisSocket = socket.socket()
+	thisSocket.bind((host,port))
+	#Listen for clients     
+	thisSocket.listen(1)
+	#Connect to client
+	conn, addr = thisSocket.accept()
+	#print Connect ip address
+	print ("The Connection ip is : " + str(addr))
+	#Repeat forever
+	while True:
+				#Receive info from client
+				receiveMess = conn.recv(1024).decode()
+				receiveWords = receiveMess.split()
+				#if no info from client end loop
+				if not receiveMess:
+									break
+				if ('search' in receiveWords or 'find' in receiveWords) and ('film' in receiveWords or 'movie' in receiveWords):
+					returnMess = str(movieSearch(receiveWords[2]))
+				elif 'search' in receiveWords or 'find' in receiveWords:
+					returnMess = 'Search Pass' 
+				else:
+					returnMess = 'No' 
+					#Print info from client
+				print ("Message from User to Chatbot : " + str(receiveMess))
+					#set return message
+				conn.send(returnMess.encode())                             
+	conn.close()                
 if __name__ == '__main__':
-            Main()
+			Main()
+
