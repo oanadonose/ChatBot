7dcd8ddfb633c496dbe7808b4fd2f8333da067b2
Luke Darragh <darraghl@coventry.ac.uk>
2017-11-15 22:54:03 +0000

    Updated the servercode to be a lot cleaner, more comments

--- server_code.py
+++ server_code.py
@@ -19,27 +19,56 @@ def Main():
 	#print Connect ip address
 	print ("The Connection ip is : " + str(addr))
 	#Repeat forever
-	filmIDSearch = 0		
+	
+	##################List of terms####################
+	searchTerms = ('search','find')
+	movieTerms = ('movie','film')
+	idTerms = ('id')		
+	
+	###################################################
 	while True:
 				#Receive info from client
-				receiveMess = conn.recv(1024).decode()
+				
+				receiveMessDecode = conn.recv(1024).decode()
+				receiveMess = receiveMessDecode.lower() #Converts input to lowercase
 				log = open ("log.txt","w")
 				log.write("\n" + "Server: " + receiveMess)
 				receiveWords = receiveMess.split()
 				#if no info from client end loop
+				
+				#################Flag Resets#################
+				filmIDSearch = 0
+				flagSearch = 0
+				flagMovie = 0
+				flagID = 0
+				#############################################
+				
+				#If no message recieved
 				if not receiveMess:
 									break
 				
+				#################Flag Settings#################
+				if searchTerms in receiveWords:
+					flagSearch = 1
+				if movieTerms in receiveWords:
+					flagMovie = 1
+				if idTerms in receiveWords:
+					flagID = 1
+				#############################################
+
+				#################Responses#################
 				if filmIDSearch == 1: #If asking user for movie title for movieID 
 					returnMess = str(movieSearch(str(receiveMess)))
 					filmIDSearch = 0
-				elif ('search' in receiveWords or 'find' in receiveWords) and ('film' in receiveWords or 'movie' in receiveWords) and ('ID' in receiveWords): #User asking for movie ID
+				elif flagSearch == 1 or flagMovie == 1 or flagID == 1: #User asking for movie ID
 					filmIDSearch = 1 #Starts asking user for film title
 					returnMess = "What movie ID would you like to search for? "	
-				elif 'search' in receiveWords or 'find' in receiveWords: #Test code
+				elif flagSearch == 1 or flagMovie == 1: 
 					returnMess = 'Search Pass' 
 				else:
 					returnMess = 'No' #NEED TO CHANGE THIS
+				###################################################
+
 					#Print info from client
 				print ("Message from User to Chatbot : " + str(receiveMess))
 					#set return message
