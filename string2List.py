078eefc1d1ff77d19980a4843dfb84a6926de2fa
Oana Donose (donoseo) <donoseo@coventry.ac.uk>
2017-11-16 15:04:58 +0000

    deleted the year function
    forgot it there...oops!

--- string2List.py
+++ string2List.py
@@ -12,17 +12,3 @@ def stringToList(receiveMess):
 	return receiveMess
 
 
-#wordsList = (stringToList(receiveMess))
-
-#def YEAR(wordsList):
-#	for word in wordsList:
-#		if word.isdigit():
-#			word = int(word)
-#			if word>1800 and word<2100:
-#				year = word
-#			#else:
-#				#print("This is not a year we support = > " + str(word))
-#	return year
-#
-#print(YEAR(wordsList))
-
