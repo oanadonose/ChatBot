84fd491d12243aae2e27f8c3e6a94d627bb1e776
Luke Darragh <darraghl@coventry.ac.uk>
2017-11-17 10:49:52 +0000

    Added a new bunch of dictionary functions

--- castlist.py
+++ castlist.py
@@ -3,13 +3,14 @@ from MovieID import movieSearch
 
 
 """Dependent on movieSearch function, takes movie title as input and returns a list(castList) containing the top 5 cast names"""
-def castGet(movieTitle):
+def castGet(movieTitle, endVar=5):
 	i = IMDb(accessSystem='http')
 	castList = [] #Creates an empty list
 	movieID = movieSearch(movieTitle) #Pulls movieID from movieSearch func
 	movie = i.get_movie(movieID) 
-	for i in range(0,5): #Pulls first 5 actors and adds to list
+	for i in range(0,endVar): #Pulls first 5 actors and adds to list
 		castPull = movie['cast'][i]
+		
 		appendName = castPull['name']
 		castList.append(appendName)
 		castList = [x.encode('utf-8') for x in castList]
