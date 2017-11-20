5fe8d3c0d3df5e81b43c269d9da34e1b68556b8a
Luke Darragh <darraghl@coventry.ac.uk>
2017-11-20 00:10:30 +0000

    Added a new function to search for the character an actor plays

--- castlist.py
+++ castlist.py
@@ -17,10 +17,14 @@ def castGet(movieTitle, endVar=5):
 		strCast = str(castList).replace("[","").replace("]","").replace("'","")
 	return strCast
 
-"""Dependent on movieSearch function, takes movie title as input and returns castDict, a full dictionary of all cast members, with their role as the value"""
-def actorDict(movieTitle):
+"""Dependent on movieSearch function, takes 'who does ACTOR play in FILM' as input and returns their role in the film"""
+def roleSearch(receiveMess):
 	i = IMDb(accessSystem='http')
-	movieID = movieSearch(movieTitle) #Pulls movieID from movieSearch func
+	messSplit = receiveMess.split(" in ")
+	actorNamePull = str(messSplit[0])
+	messSplit = str(messSplit[1])
+	actorNamePull = actorNamePull.replace("who ","").replace("does ","").replace(" play","")
+	movieID = movieSearch(messSplit) #Pulls movieID from movieSearch func
 	movie = i.get_movie(movieID) 
 	castDict = {} #Creates empty dictionary
 	cast = movie.get('cast') 
@@ -30,7 +34,26 @@ def actorDict(movieTitle):
 			actorRole = actor.currentRole
 			castDict ["{0}".format(actor['name'])] = "{0}".format(actorRole) #Adds entries to dictionary with actor name as key, role as value. 
 	finally:
-		return castDict
+		return str(castDict[actorNamePull])
+
+"""Dependent on movieSearch function, takes movie title as input and returns castDict, a full dictionary of all cast members, with their role as the value"""
+def actorDict(receiveMess):
+	i = IMDb(accessSystem='http')
+	messSplit = receiveMess.split(" in ")
+	actorNamePull = str(messSplit[0])
+	messSplit = str(messSplit[1])
+	actorNamePull = actorNamePull.replace("who ","").replace("does ","").replace(" play","")
+	movieID = movieSearch(messSplit) #Pulls movieID from movieSearch func
+	movie = i.get_movie(movieID) 
+	castDict = {} #Creates empty dictionary
+	cast = movie.get('cast') 
+	try: #Pulls all actors out of the film until it fails.
+		for actor in cast[:9999]:
+			actorName = actor['name']
+			actorRole = actor.currentRole
+			castDict ["{0}".format(actor['name'])] = "{0}".format(actorRole) #Adds entries to dictionary with actor name as key, role as value. 
+	finally:
+		return str(castDict[actorNamePull])
 
 """Dependent on movieSearch function, takes movie title as input and returns roleDict, a full dictionary of all roles, with their actor as the value"""
 def roleDict(movieTitle):
