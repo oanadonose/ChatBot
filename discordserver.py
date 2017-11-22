fdd95fad8e3292171d845886efb0a1150c2c766d
Luke Darragh <darraghl@coventry.ac.uk>
2017-11-22 12:06:26 +0000

    Added better error catching and actor from role search

--- discordserver.py
+++ discordserver.py
@@ -51,50 +51,56 @@ def on_message(message):
 	if message.author == client.user:
 		return
 	else:
-		#################Flag Settings#################
-		if any(word in receiveWords for word in gV.searchTerms):
-			gV.flagSearch = 1
-		if any(word in receiveWords for word in gV.movieTerms):
-			gV.flagMovie = 1
-		if any(word in receiveWords for word in gV.idTerms):
-			gV.flagID = 1
-		if any(word in receiveWords for word in gV.castTerms):
-			gV.flagCast = 1
-		if any(word in receiveWords for word in gV.whoTerms):
-			gV.flagWho = 1
-		if any(word in receiveWords for word in gV.playTerms):
-			gV.flagPlay = 1
-		#############################################
-		if gV.filmIDSearch == 1: #If asking user for movie title for movieID 
-			returnMess = str(movieSearch(receiveMess))
-			gV.filmIDSearch = 0
-		elif gV.castNumPull == 1:
-			gV.castNumPull = 0
-			gV.castSearch = 1
-			gV.titleStore = receiveMess
-			returnMess = "How many cast members do you want listed? "
-		elif gV.castSearch == 1:
-			castNum = receiveMess
-			returnMess = str(castGet(gV.titleStore,int(castNum)))
-			gV.castSearch = 0
-			gV.flagSearch = 0
-			gV.flagCast = 0
-		elif gV.flagWho == 1 and gV.flagPlay ==1: #If ask for specific actor's role in film
-			returnMess = (str(roleSearch(receiveMess)))
-			gV.flagWho = 0
-			gV.flagPlay = 0
-		elif gV.flagSearch == 1 and gV.flagMovie == 1 and gV.flagID == 1: #User asking for movie ID
-			gV.filmIDSearch = 1 #Starts asking user for film title
-			returnMess = "What movie ID would you like to search for? " 
-		elif gV.flagSearch == 1 and gV.flagCast == 1: 
-			gV.castNumPull = 1
-			returnMess = "What movie would you like to search for the cast members of?"
-		elif gV.flagSearch == 1 and gV.flagMovie == 1: 
-			returnMess = 'Search Pass' 
-		elif receiveMess == 'debug':
-			returnMess = receiveMess + receiveWords
-		else:
-			returnMess = "I'm sorry, I didn't understand." #Error catch
+		try:
+			#################Flag Settings#################
+			if any(word in receiveWords for word in gV.searchTerms):
+				gV.flagSearch = 1
+			if any(word in receiveWords for word in gV.movieTerms):
+				gV.flagMovie = 1
+			if any(word in receiveWords for word in gV.idTerms):
+				gV.flagID = 1
+			if any(word in receiveWords for word in gV.castTerms):
+				gV.flagCast = 1
+			if any(word in receiveWords for word in gV.whoTerms):
+				gV.flagWho = 1
+			if any(word in receiveWords for word in gV.playTerms):
+				gV.flagPlay = 1
+			#############################################
+			if gV.filmIDSearch == 1: #If asking user for movie title for movieID 
+				returnMess = str(movieSearch(receiveMess))
+				gV.filmIDSearch = 0
+			elif gV.castNumPull == 1:
+				gV.castNumPull = 0
+				gV.castSearch = 1
+				gV.titleStore = receiveMess
+				returnMess = "How many cast members do you want listed? "
+			elif gV.castSearch == 1:
+				castNum = receiveMess
+				returnMess = str(castGet(gV.titleStore,int(castNum)))
+				gV.castSearch = 0
+				gV.flagSearch = 0
+				gV.flagCast = 0
+			elif gV.flagWho == 1 and gV.flagPlay ==1: #If ask for specific actor's role in film
+				returnMess = (str(roleSearch(receiveMess)))
+				gV.flagWho = 0
+				gV.flagPlay = 0
+			elif gV.flagSearch == 1 and gV.flagMovie == 1 and gV.flagID == 1: #User asking for movie ID
+				gV.filmIDSearch = 1 #Starts asking user for film title
+				returnMess = "What movie ID would you like to search for? " 
+			elif gV.flagSearch == 1 and gV.flagCast == 1: 
+				gV.castNumPull = 1
+				returnMess = "What movie would you like to search for the cast members of?"
+			elif gV.flagSearch == 1 and gV.flagMovie == 1: 
+				returnMess = 'Search Pass' 
+			elif receiveMess == 'debug':
+				returnMess = receiveMess + receiveWords
+			else:
+				returnMess = "I'm sorry, I didn't understand." #Error catch
+		except Exception as e :
+			e.args = (e.args[0] + ' happened',)
+			returnMess = "An error occured"
+			yield from client.send_message(message.channel, returnMess)
+			raise
 
 
 		yield from client.send_message(message.channel, returnMess)
