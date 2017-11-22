60dc818ac199da05c033d1b59257d3ea27b8affd
donoseo <donoseo@uni.coventry.ac.uk>
2017-11-22 17:22:01 +0000

    changed from title input to object input

--- producerfunc.py
+++ producerfunc.py
@@ -2,11 +2,14 @@ from imdb import IMDb
 from MovieID import movieSearch
 ia = IMDb()
 
-def producerGet(movieTitle):
-	"""Takes movie title as input and returns a tuple of the no. of producers and a list containing all producers' names"""
+#movieTitle = input("input title : ")
+
+#movieID = movieSearch(movieTitle)
+#movie = ia.get_movie(movieID)
+
+def producerGet(movie):
+	"""Takes movie OBJECT as input and returns a tuple of the no. of producers and a list containing all producers' names"""
 	producerListStr = []
-	movieID = movieSearch(movieTitle)
-	movie = ia.get_movie(movieID)
 	producerList = movie['producer']
 	numberOfproducers = len(producerList)
 	for i in range(0,numberOfproducers):
--- writerfunc.py
+++ writerfunc.py
@@ -2,11 +2,14 @@ from imdb import IMDb
 from MovieID import movieSearch
 ia = IMDb()
 
-def writerGet(movieTitle):
-	"""Takes movie title as input and returns a tuple of the no. of writers and a list containing all writers' names"""
+#movieTitle = input("input title : ")
+
+#movieID = movieSearch(movieTitle)
+#movie = ia.get_movie(movieID)
+
+def writerGet(movie):
+	"""Takes movie OBJECT as input and returns a tuple of the no. of writers and a list containing all writers' names"""
 	writerListStr = []
-	movieID = movieSearch(movieTitle)
-	movie = ia.get_movie(movieID)
 	writerList = movie['writer']
 	numberOfWriters = len(writerList)
 	for i in range(0,numberOfWriters):
