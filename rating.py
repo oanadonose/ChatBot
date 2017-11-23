80f21b051f3820cf23fb728a6653888f88edcbc4
Rob <green27@uni.coventry.ac.uk>
2017-11-23 15:39:29 +0000

    fixed empty list problem

--- rating.py
+++ rating.py
@@ -1,23 +1,39 @@
 from imdb import IMDb
+import random
 
 i = IMDb()
 def getRating(movie):
-	"""Asks the user for a movie title as input and returns the title and IMDB rating"""
+	"""Takes a movie title as input and returns the title and IMDB rating"""	
 	movieList = i.search_movie(movie)
 	firstMatch = movieList[0]
 	i.update(firstMatch)
-	return "%s : %s" % (firstMatch['title'], firstMatch['rating'])
+	return("%s : %s" % (firstMatch['title'], firstMatch['rating']))
 
-def searchKeyword(keyword):
+def searchKeyword(receiveMess):
 	"""Takes a keyword as input, and searches for films containing that keyword, returning their title and rating"""
-	movieList = i.get_keyword(keyword)
-	while True:
-		x=0
-		film = movieList[x]
-		print("I recommend "+ film['title']+", with a rating of "+ film['rating'])
-		user = input("Would you like another recommendation? y/n")
-		if user == "y":
-			x = x+1
+	inputList = receiveMess.split()
+	for n in range(0, len(inputList)-1):
+		if inputList[n] == "about":
+			keyword = inputList[n+1]
+			break #selects the keyword as the word after "about" or "involving" in the input (i.e "movies ABOUT zombies")
 		else:
+			continue
+	movieList = i.get_keyword(keyword) #returns a list of movies that contain a given keyword
+	x = 0
+	yesResponses = ["yes", "yeah", "okay", "y"]
+	noResponses = ["no", "nope", "no thanks", "nah", "n"]
+	while True:
+		film = movieList[x]
+		i.update(film)
+		print("I recommend "+ film['title'] + ", with a rating of "+ str(film['rating']))
+		user = input("Would you like another recommendation? ")
+		if user.lower() in yesResponses: #loop iterates again, this time printing the next movie in movieList
+			x = x+1
+		elif user.lower() in noResponses:
 			break
-#the get_keyword function appears to be returning an empty list, so for the time being searchKeyword is unfinished
+		else:
+			print("I'm afraid I don't understand") #Breaks the while loop in the event of an unexpected input
+			break
+
+#print(getRating("avatar"))
+searchKeyword('find me a film about cars')
