205e5766de92b026e4842b3bf7871e5f23b52d8f
Luke Darragh (darraghl) <darraghl@coventry.ac.uk>
2017-11-23 21:10:50 +0000

    Made it work.

--- discordserver.py
+++ discordserver.py
@@ -7,8 +7,8 @@ from castlist import castGet
 from castlist import roleSearch
 from moreInformation2 import moreInfo
 from tvSeries import seasonsEpisodesCounter, listOfEpisodes, infoAboutEpisode
-from directorrr import directorGet
-from Companyinfo import companyInfo
+#from directorrr import directorGet
+#from Companyinfo import companyInfo
 
 client = discord.Client()
 class gV(): #Defines the class of globalVariables, must start any refernece to these variables with gV.
@@ -21,7 +21,7 @@ class gV(): #Defines the class of globalVariables, must start any refernece to t
 	playTerms = ['play']
 	tvSeriesTerms = ['series','tvseries','show']
 	directorTerms = ['director','directed']
-        companyTerms=['company']
+	companyTerms=['company']
 	##################################################
 
 	####################Defaults######################
@@ -37,7 +37,7 @@ class gV(): #Defines the class of globalVariables, must start any refernece to t
 	episodeNumber = 0
 	getdire = 0
 	getTop = 0
-        company = 0
+	company = 0
 	##################################################
 
 
@@ -58,9 +58,9 @@ class gV(): #Defines the class of globalVariables, must start any refernece to t
 	flagSeriesSeason = 0
 	flagSeriesEpisode = 0
 	flagReco = 0
-        flagDirect = 0
-        flagTop = 0
-        flagCompany = 0
+	flagDirect = 0
+	flagTop = 0
+	flagCompany = 0
 	#############################################
 
 @client.event #Prints a ready message to terminal
@@ -97,26 +97,26 @@ def on_message(message):
 			if any(word in receiveWords for word in gV.tvSeriesTerms or gV.episodeTerms):
 				gV.flagSeries = 1
 			if any(word in receiveWords for word in gV.directorTerms):
-                                gV.flagDirect = 1
-                        if any(word in receiveWords for word in gV.companyTerms):
-                                gV.flagCompany = 1
+				gV.flagDirect = 1
+			if any(word in receiveWords for word in gV.companyTerms):
+				gV.flagCompany = 1
 			#############################################
 			if gV.filmIDSearch == 1: #If asking user for movie title for movieID 
 				returnMess = str(movieSearch(receiveMess))
 				gV.filmIDSearch = 0
 			elif gV.flagDirect == 1:
-                		gV.getdire=1
-                		returnMess="What movie are you interested to know the director?InputID"	
+				gV.getdire=1
+				returnMess="What movie are you interested to know the director?InputID"	
 			elif gV.flagCompany == 1:
-                		returnMess="What movie are your looking for company info?"
-                		gV.company = 1
-                		gV.flagCompany =0
-            		elif gV.company == 1:
-               			 returnMess=str(companyInfo(receiveMess))
-              			gV.company=0
-          	        elif gV.getdire == 1:
-               			 returnMess =str(directorGet(receiveMess))
-               		  	gV.getdire =0	
+				returnMess="What movie are your looking for company info?"
+				gV.company = 1
+				gV.flagCompany =0
+			elif gV.company == 1:
+				eturnMess=str(companyInfo(receiveMess))
+				gV.company=0
+			elif gV.getdire == 1:
+				returnMess =str(directorGet(receiveMess))
+				gV.getdire =0	
 			elif gV.castNumPull == 1:
 				gV.castNumPull = 0
 				gV.castSearch = 1
