b22e8ce1ffacd6beedc42f97c4407d15ad848c0d
Luke Darragh <darraghl@coventry.ac.uk>
2017-11-24 13:38:42 +0000

    Removed a load of Head things again

--- castlist.py
+++ castlist.py
@@ -1,38 +1,7 @@
-#<<<<<<< HEAD
+
 from imdb import IMDb
 from MovieID import movieSearch
 ia = IMDb()
-
-"""Takes a movie title as input, runs a search and returns the movieID for the first match in the list"""
-
-#Commented out for testing purposes
-'''def movieSearch(movieTitle):
-	for movie in ia.search_movie(movieTitle):
-		results = ia.search_movie(movieTitle)
-		first = results[0]
-		return first.movieID
-		break'''
-
-"""Dependent on movieSearch function, takes movie title as input and returns a list(castList) containing the top 5 cast names"""
-def castGet(movieTitle):
-	castList = [] #Creates an empty list
-	movieID = movieSearch(movieTitle) #Pulls movieID from movieSearch func
-	movie = ia.get_movie(movieID) 
-	for i in range(0,5): #Pulls first 5 actors and adds to list
-		castPull = movie['cast'][i]
-		appendName = castPull['name']
-		castList.append(appendName)
-	return castList
-
-#Testing
-#title = input('Input Title ') 
-#test = movieSearch(title)
-#print (test)
-#print (castGet(title))
-#=======
-from imdb import IMDb
-from MovieID import movieSearch
-
 
 """Dependent on movieSearch function, takes movie title as input and returns a list(castList) containing the top 5 cast names"""
 def castGet(movieTitle, endVar=5):
@@ -113,4 +82,3 @@ def roleSearch(receiveMess):
 		actorPull = actorPull.replace("who ","").replace("does ","").replace(" play","")
 		actorDictPull = actorDict(filmPull)
 		return str(actorDictPull[actorPull])
-#>>>>>>> 14c07d478f719a90b356c870139fd1569e762367
