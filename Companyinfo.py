795e690cdc6b374a39627446738d6d37d9f50bf0
Andreea Ghinies (ghiniesa) <ghiniesa@coventry.ac.uk>
2017-11-24 12:29:28 +0000

    updated company info

--- Companyinfo.py
+++ Companyinfo.py
@@ -1,20 +1,17 @@
+
 from imdb import IMDb
 ia = IMDb()
-r=input("Please insert a movie name")
-s = ia.search_movie('Deadpool')
-dp = s[0]
-ia.update(dp)
-print (dp.get('production companies'))
+#r=input("Please insert a movie name")
+"""Takes a movie title as input and returns production company info"""
 
-movie = ia.get_movie('1375666')
-actor = movie['cast']
-#Printing the first 2 persons of cast
-print ("Cast: ")
-for i in actor[:2]:
-    for j in ia.search_person(str(i))[:1]:
-        print (i, j.personID)
-#Gives other movies in which the first actor played.       
-full_person = ia.get_person(actor[0].getID(), info=["filmography"])
-full_person.keys()
-print(full_person["actor"])
-
+#deadpool
+def companyInfo(movie):
+    strresult=""
+    s = ia.search_movie(movie)
+    dp = s[0]
+    ia.update(dp)
+    result=dp.get('production companies')
+    for i in range(len(result)):
+        strresult=strresult+ "," +str(result[i]['name'])
+    return (strresult)
+#print (companyInfo("deadpool"))
