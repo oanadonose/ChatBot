4744729bf87af1938976fc04c7a0cb378fe76877
Ibukunoluwa Adegoke (adegokei) <adegokei@coventry.ac.uk>
2017-11-24 16:26:30 +0000

    Update CHARACTER.py

--- CHARACTER.py
+++ CHARACTER.py
@@ -1,20 +1,20 @@
 from imdb import IMDb
-ia = IMDb() 
-imdb_access = imdb.IMDb()
+ ia = IMDb() 
+ imdb_access = imdb.IMDb()
 
 def summary(self) :
  """"This returns a string with a summary for the character """
 if not self:
-      return ''
+   return ''
       
 s = 'Character\n=====\nName: %s\n' % self.get('name','')
 bio = self.get('biography')
-if bio:
+  if bio:
     s += 'Biography: %s\n' % bio[0]
 filmo = self.get('filmography')
-if filmo:
+  if filmo:
     a_list = [x.get('long imdb canonical title','')]
 
-for x in filmo[:5]]
+ for x in filmo[:5]]
 s += 'Last movies with this character: %s. \n' % '; '.join(a_list)
 return s 
